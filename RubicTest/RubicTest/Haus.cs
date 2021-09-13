0ca5662ba0e739b7af88fb68e4588659ae75acb0
Sashagusew <90207484+Sashagusew@users.noreply.github.com>
2021-09-13 19:27:47 +0300

    Иерархия

---
+++ RubicTest/RubicTest/Haus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicTest
+{
+    class Haus
+    {
+        private string street = "Boldina";
+        private int width = 20;
+        private float foundation = 123.13F;
+        private bool old = true;
+
+    }
+}
